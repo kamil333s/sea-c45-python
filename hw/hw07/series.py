d68494b652b5f13e709622841cb91f6d9503f38b
Kevin Smith <kamil@sisna.com>
2015-09-16 20:49:19 -0700

    Add initial homework with Fibonacci skeleton.

---
+++ hw/hw07/series.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    """
+    Returns the nth number from the Fibonacci sequence.
+
+    Example: fibonacci(0) = 0
+                   fibonacci(1) = 1
+    """
+
+    Return 1
+    
