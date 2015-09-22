a68ed105ac81b83bc7203ccec2a70176ebcde1a3
Kevin Smith <kamil@sisna.com>
2015-09-21 20:16:45 -0700

    Add initial homework file.

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -6,5 +6,43 @@ def fibonacci(n):
                    fibonacci(1) = 1
     """
 
-    Return 1
-    
+    if (n == 0):
+        return 0
+    elif (n == 1):
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def lucas(n):
+    """
+    Returns the nth number from the Lucas sequence.
+
+    Example: lucas(0) = 2
+                   lucas(1) = 1
+                   ...
+                   lucas(n-1) + lucas(n-2)
+    """
+    if (n == 0):
+        return 2
+    elif (n == 1):
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
+
+
+def sum_series(n, zeroth=0, oneth=1):
+    """
+
+        Example: sum_series(0) == neroth
+                        sum_series(1) == oneth
+                        ...
+                        sum_series(n) == sum_series(n - 1) + sum_series(n - )
+    """
+    if (n == 0):
+        return zeroth
+    elif (n == 1):
+        return oneth
+    else:
+        return sum_series(n - 1, zeroth, oneth) + \
+            sum_series(n - 2, zeroth, oneth)           
