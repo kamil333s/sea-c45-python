48d0175ab3ac8bc59adf1bf2656605a5266a9a99
Kevin Smith <kamil@sisna.com>
2015-09-22 12:41:14 -0700

    Create initial homework file

---
+++ hw/hw04/break_me.py
@@ -0,0 +1,21 @@
+import math
+
+
+def oops_name():
+    print(myfunc())
+
+oops_name()
+
+
+def oops_type(num):
+    y = math.sin(num)
+    print(y)
+
+oops_type("Pie")
+
+
+def oops_attribute():
+    z = math.newfunction()
+    print(z)
+
+oops_attribute()
